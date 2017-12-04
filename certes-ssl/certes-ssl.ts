3b5413f7e18019d4f0e795565ec971f54279d375
Eddie Lin <fszlin@gmail.com>
2017-12-04 16:05:57 -0500

    add indetifiers var

--- certes-ssl/certes-ssl.ts
+++ certes-ssl/certes-ssl.ts
@@ -1,17 +1,18 @@
 import tl = require('vsts-task-lib/task');
+import trm = require('vsts-task-lib/toolrunner');
 //npm install vsts-task-lib
 
 // Get task parameters
 let variable1: string = tl.getPathInput('variable1', false, true);
 let variable2: string = tl.getInput('variable2', true);
-
-
+let identifiers = tl.getInput('Identifiers', true);
 
 async function run() {
     try {
         //do your actions
-        tl.debug('variable1:' +variable1)
-        tl.debug('variable2:' +variable2)
+        tl.debug('variable1:' +variable1);
+        tl.debug('variable2:' +variable2);
+        tl.debug('identifiers:' +identifiers);
         
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
